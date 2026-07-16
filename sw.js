f9446daa27a78d47131f9938a1b8f4acf8f9ffb6
Ranjan Singh <ranzansingh29@gmail.com>
2026-07-16 07:35:19 +0545

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this when index.html changes so clients pick up the new shell.
-var CACHE_NAME = 'ludo-shell-v3';
+var CACHE_NAME = 'ludo-shell-v4';
 var SHELL_FILES = [
   './',
   './index.html',
